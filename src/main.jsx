b4d24c6aa2c8b93e6c37a75f1c037bdad619916f
sonymounika63 <sonymounika63@gmail.com>
2025-09-17 12:52:45 +0530

    Build: output to docs for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from "react-router-dom";
 import './index.css'
 import './App.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </StrictMode>,
-)
+  </HashRouter>
+);
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Gallery-React/',
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "docs",              // build output goes to /docs on main
+    emptyOutDir: true,           // clean docs/ before each build
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
